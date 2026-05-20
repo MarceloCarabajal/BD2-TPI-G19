8fee1fc5d74a6b6cbebb581be48067822978088e
MarceloCarabajal <marce.tsgia@gmail.com>
2026-05-20 19:43:36 -0300

    chore: se actualizan los nombres de las tablas con mayusculas y en pulural

--- sql/desarrollo_db/creacion_bd.sql
+++ sql/desarrollo_db/creacion_bd.sql
@@ -15,23 +15,29 @@ GO
 SET DATEFORMAT ymd;
 GO
 
+-- USE DBTutorias
+-- DROP DATABASE IF EXISTS BD2_TPI_G19;
 -- gaston
+GO
 
-CREATE TABLE CLASIFICACIONES (
+CREATE TABLE CLASIFICACIONES
+(
     id_clasificacion INT NOT NULL IDENTITY(1, 1),
     descripcion VARCHAR(50) NOT NULL,
     CONSTRAINT PK_CLASIFICACIONES PRIMARY KEY (id_clasificacion)
 );
 GO
 
-CREATE TABLE GENEROS (
+CREATE TABLE GENEROS
+(
     id_genero INT NOT NULL IDENTITY(1, 1),
     descripcion VARCHAR(50) NOT NULL,
     CONSTRAINT PK_GENEROS PRIMARY KEY (id_genero)
 );
 GO
 
-CREATE TABLE PELICULAS (
+CREATE TABLE PELICULAS
+(
     id_pelicula INT NOT NULL IDENTITY(1, 1),
     id_clasificacion INT NOT NULL,
     id_genero INT NOT NULL,
@@ -48,48 +54,52 @@ GO
 
 -- gisela
 
-CREATE TABLE Complejos (
+CREATE TABLE COMPLEJOS
+(
     id_complejo INT NOT NULL IDENTITY(1, 1),
     nombre VARCHAR(100) NOT NULL,
     direccion VARCHAR(255) NOT NULL,
     telefono VARCHAR(50) NULL,
-    CONSTRAINT PK_Complejos PRIMARY KEY (id_complejo)
+    CONSTRAINT PK_COMPLEJOS PRIMARY KEY (id_complejo)
 );
 GO
 
-CREATE TABLE Salas (
+CREATE TABLE SALAS
+(
     id_sala INT NOT NULL IDENTITY(1, 1),
     id_complejo INT NOT NULL,
     nombre_sala VARCHAR(50) NOT NULL,
     capacidad_total INT NOT NULL,
     tipo_sala VARCHAR(10) NOT NULL,
-    CONSTRAINT PK_Salas PRIMARY KEY (id_sala),
-    CONSTRAINT FK_Salas_Complejos
-        FOREIGN KEY (id_complejo) REFERENCES Complejos (id_complejo),
-    CONSTRAINT CK_Salas_TipoSala CHECK (tipo_sala IN ('2D', '3D', 'IMAX')),
-    CONSTRAINT CK_Salas_Capacidad CHECK (capacidad_total > 0)
+    CONSTRAINT PK_SALAS PRIMARY KEY (id_sala),
+    CONSTRAINT FK_SALAS_COMPLEJOS
+        FOREIGN KEY (id_complejo) REFERENCES COMPLEJOS (id_complejo),
+    CONSTRAINT CK_SALAS_TIPO_SALA CHECK (tipo_sala IN ('2D', '3D', 'IMAX')),
+    CONSTRAINT CK_SALAS_CAPACIDAD CHECK (capacidad_total > 0)
 );
 GO
 
-CREATE TABLE Funciones (
+CREATE TABLE FUNCIONES
+(
     id_funcion INT NOT NULL IDENTITY(1, 1),
     id_pelicula INT NOT NULL,
     id_sala INT NOT NULL,
     fecha_hora DATETIME NOT NULL,
     precio_base DECIMAL(10, 2) NOT NULL,
-    CONSTRAINT PK_Funciones PRIMARY KEY (id_funcion),
-    CONSTRAINT FK_Funciones_Salas
-        FOREIGN KEY (id_sala) REFERENCES Salas (id_sala),
-    CONSTRAINT FK_Funciones_Peliculas
+    CONSTRAINT PK_FUNCIONES PRIMARY KEY (id_funcion),
+    CONSTRAINT FK_FUNCIONES_SALAS
+        FOREIGN KEY (id_sala) REFERENCES SALAS (id_sala),
+    CONSTRAINT FK_FUNCIONES_PELICULAS
         FOREIGN KEY (id_pelicula) REFERENCES PELICULAS (id_pelicula),
-    CONSTRAINT CK_Funciones_PrecioBase CHECK (precio_base > 0),
-    CONSTRAINT CK_Funciones_FechaHora CHECK (fecha_hora > GETDATE())
+    CONSTRAINT CK_FUNCIONES_PRECIO_BASE CHECK (precio_base > 0),
+    CONSTRAINT CK_FUNCIONES_FECHA_HORA CHECK (fecha_hora > GETDATE())
 );
 GO
 
 -- henry (parte 1)
 
-CREATE TABLE USUARIOS (
+CREATE TABLE USUARIOS
+(
     id_usuario INT NOT NULL IDENTITY(1, 1),
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100) NOT NULL,
@@ -101,13 +111,14 @@ CREATE TABLE USUARIOS (
 );
 GO
 
-CREATE TABLE BUTACAS (
+CREATE TABLE BUTACAS
+(
     id_butaca INT NOT NULL IDENTITY(1, 1),
     id_sala INT NOT NULL,
     fila VARCHAR(1) NOT NULL,
     numero TINYINT NOT NULL,
     CONSTRAINT PK_BUTACAS PRIMARY KEY (id_butaca),
-    CONSTRAINT FK_BUTACAS_SALAS FOREIGN KEY (id_sala) REFERENCES Salas (id_sala),
+    CONSTRAINT FK_BUTACAS_SALAS FOREIGN KEY (id_sala) REFERENCES SALAS (id_sala),
     CONSTRAINT CK_BUTACAS_Numero CHECK (numero >= 1),
     CONSTRAINT UQ_BUTACAS_SalaFilaNumero UNIQUE (id_sala, fila, numero)
 );
@@ -115,59 +126,63 @@ GO
 
 -- marcelo
 
-CREATE TABLE Metodos_Pagos (
+CREATE TABLE METODOS_PAGOS
+(
     id_metodo_pago INT NOT NULL IDENTITY(1, 1),
     nombre VARCHAR(50) NOT NULL,
-    CONSTRAINT PK_Metodos_Pagos PRIMARY KEY (id_metodo_pago),
-    CONSTRAINT UQ_Metodos_Pagos_Nombre UNIQUE (nombre)
+    CONSTRAINT PK_METODOS_PAGOS PRIMARY KEY (id_metodo_pago),
+    CONSTRAINT UQ_METODOS_PAGOS_Nombre UNIQUE (nombre)
 );
 GO
 
-CREATE TABLE Reservas (
+CREATE TABLE RESERVAS
+(
     id_reserva INT NOT NULL IDENTITY(1, 1),
     id_usuario INT NOT NULL,
     id_funcion INT NOT NULL,
     fecha_reserva DATETIME NOT NULL DEFAULT GETDATE(),
     total_pagado DECIMAL(10, 2) NULL,
     estado VARCHAR(20) NOT NULL DEFAULT 'Pendiente',
-    CONSTRAINT PK_Reservas PRIMARY KEY (id_reserva),
-    CONSTRAINT FK_Reservas_Usuarios FOREIGN KEY (id_usuario) REFERENCES USUARIOS (id_usuario),
-    CONSTRAINT FK_Reservas_Funciones FOREIGN KEY (id_funcion) REFERENCES Funciones (id_funcion),
-    CONSTRAINT CK_Reservas_Estado CHECK (estado IN ('Pendiente', 'Pagada', 'Cancelada')),
-    CONSTRAINT CK_Reservas_TotalPagado CHECK (total_pagado IS NULL OR total_pagado >= 0)
+    CONSTRAINT PK_RESERVAS PRIMARY KEY (id_reserva),
+    CONSTRAINT FK_RESERVAS_USUARIOS FOREIGN KEY (id_usuario) REFERENCES USUARIOS (id_usuario),
+    CONSTRAINT FK_RESERVAS_FUNCIONES FOREIGN KEY (id_funcion) REFERENCES FUNCIONES (id_funcion),
+    CONSTRAINT CK_RESERVAS_ESTADO CHECK (estado IN ('Pendiente', 'Pagada', 'Cancelada')),
+    CONSTRAINT CK_RESERVAS_TOTALPAGADO CHECK (total_pagado IS NULL OR total_pagado >= 0)
 );
 GO
 
-CREATE TABLE Pagos (
+CREATE TABLE PAGOS
+(
     id_pago INT NOT NULL IDENTITY(1, 1),
     id_reserva INT NOT NULL,
     id_metodo_pago INT NOT NULL,
     fecha_pago DATETIME NOT NULL DEFAULT GETDATE(),
     total_pagado DECIMAL(10, 2) NOT NULL,
     estado_pago VARCHAR(20) NOT NULL DEFAULT 'Pendiente',
-    CONSTRAINT PK_Pagos PRIMARY KEY (id_pago),
-    CONSTRAINT FK_Pagos_Reservas FOREIGN KEY (id_reserva) REFERENCES Reservas (id_reserva),
-    CONSTRAINT FK_Pagos_Metodos_Pagos FOREIGN KEY (id_metodo_pago) REFERENCES Metodos_Pagos (id_metodo_pago),
-    CONSTRAINT UQ_Pagos_Reserva UNIQUE (id_reserva),
-    CONSTRAINT CK_Pagos_TotalPagado CHECK (total_pagado > 0),
-    CONSTRAINT CK_Pagos_Estado CHECK (estado_pago IN ('Pendiente', 'Aprobado', 'Rechazado', 'Devuelto'))
+    CONSTRAINT PK_PAGOS PRIMARY KEY (id_pago),
+    CONSTRAINT FK_PAGOS_RESERVAS FOREIGN KEY (id_reserva) REFERENCES RESERVAS (id_reserva),
+    CONSTRAINT FK_PAGOS_METODOS_PAGOS FOREIGN KEY (id_metodo_pago) REFERENCES METODOS_PAGOS (id_metodo_pago),
+    CONSTRAINT UQ_PAGOS_RESERVA UNIQUE (id_reserva),
+    CONSTRAINT CK_PAGOS_TOTALPAGADO CHECK (total_pagado > 0),
+    CONSTRAINT CK_PAGOS_ESTADO CHECK (estado_pago IN ('Pendiente', 'Aprobado', 'Rechazado', 'Devuelto'))
 );
 GO
 
 -- henry (parte 2)
 
-CREATE TABLE DETALLE_RESERVA (
+CREATE TABLE DETALLES_RESERVAS
+(
     id_detalle INT NOT NULL IDENTITY(1, 1),
     id_reserva INT NOT NULL,
     id_butaca INT NOT NULL,
     id_funcion INT NOT NULL,
     precio_unitario MONEY NOT NULL,
-    CONSTRAINT PK_DETALLE_RESERVA PRIMARY KEY (id_detalle),
-    CONSTRAINT FK_DETALLE_RESERVA_RESERVAS FOREIGN KEY (id_reserva) REFERENCES Reservas (id_reserva),
-    CONSTRAINT FK_DETALLE_RESERVA_BUTACAS FOREIGN KEY (id_butaca) REFERENCES BUTACAS (id_butaca),
-    CONSTRAINT FK_DETALLE_RESERVA_FUNCIONES FOREIGN KEY (id_funcion) REFERENCES Funciones (id_funcion),
-    CONSTRAINT CK_DETALLE_RESERVA_Precio CHECK (precio_unitario >= 0),
-    CONSTRAINT UQ_DETALLE_RESERVA_FuncionButaca UNIQUE (id_funcion, id_butaca)
+    CONSTRAINT PK_DETALLES_RESERVAS PRIMARY KEY (id_detalle),
+    CONSTRAINT FK_DETALLES_RESERVAS_RESERVAS FOREIGN KEY (id_reserva) REFERENCES RESERVAS (id_reserva),
+    CONSTRAINT FK_DETALLES_RESERVAS_BUTACAS FOREIGN KEY (id_butaca) REFERENCES BUTACAS (id_butaca),
+    CONSTRAINT FK_DETALLES_RESERVAS_FUNCIONES FOREIGN KEY (id_funcion) REFERENCES FUNCIONES (id_funcion),
+    CONSTRAINT CK_DETALLES_RESERVAS_Precio CHECK (precio_unitario >= 0),
+    CONSTRAINT UQ_DETALLES_RESERVAS_FuncionButaca UNIQUE (id_funcion, id_butaca)
 );
 GO
 
