63d8cf8e46bc12ece4bb04c59a48f843f7cc131d
615374 <giselalanzillotta@gmail.com>
2026-05-24 13:46:07 -0300

    feat: agregar inserts para complejos, salas y funciones

--- sql/desarrollo_db/insercion_datos.sql
+++ sql/desarrollo_db/insercion_datos.sql
@@ -5,6 +5,48 @@ GO
 
 -- Gaston
 -- Gisela
+
+-- Gisela
+
+-- INSERTS: COMPLEJOS 
+
+INSERT INTO COMPLEJOS (nombre, direccion, telefono) VALUES
+    ('CineHoyts Abasto', 'Av. Corrientes 3247, CABA', '11-4861-2200'),
+    ('Cinépolis Recoleta', 'Vicente López 2050, CABA', '11-4808-0000'),
+    ('Showcase Belgrano', 'Av. Monroe 1655, CABA', '11-4789-9100'),
+    ('Multiplex Belgrano', 'Vuelta de Obligado 2199, CABA', '11-4781-5500'),
+    ('Cinema Devoto', 'José Pedro Varela 4866, CABA', '11-4505-8000');
+GO
+
+SELECT * FROM COMPLEJOS;
+
+
+-- INSERTS: SALAS (Mínimo 5 - Distribuidas en los complejos anteriores)
+
+INSERT INTO SALAS (id_complejo, nombre_sala, capacidad_total, tipo_sala) VALUES
+    (1, 'Sala 1 - IMAX', 300, 'IMAX'),
+    (1, 'Sala 2 - 3D', 180, '3D'),
+    (2, 'Sala Monster 2D', 220, '2D'),
+    (3, 'Sala 3D Belgrano', 150, '3D'),
+    (4, 'Sala 4 - 2D', 120, '2D'),
+    (5, 'Sala Comfort 3D', 200, '3D');
+GO
+
+SELECT * FROM SALAS;
+
+-- INSERTS: FUNCIONES
+
+INSERT INTO FUNCIONES (id_pelicula, id_sala, fecha_hora, precio_base) VALUES
+    (1, 1, '2026-06-15 18:00:00', 4500.00), -- Película 1 en Sala 1 (IMAX)
+    (2, 2, '2026-06-15 21:30:00', 3500.00), -- Película 2 en Sala 2 (3D)
+    (3, 3, '2026-06-16 15:00:00', 2800.00), -- Película 3 en Sala 3 (2D)
+    (4, 4, '2026-06-16 20:00:00', 3500.00), -- Película 4 en Sala 4 (3D)
+    (5, 5, '2026-06-17 19:15:00', 2500.00), -- Película 5 en Sala 5 (2D)
+    (1, 6, '2026-06-17 22:00:00', 3500.00); -- Película 1 en Sala 6 (3D)
+GO
+
+SELECT * FROM FUNCIONES;
+
 -- Henry
 
 -- Marce
