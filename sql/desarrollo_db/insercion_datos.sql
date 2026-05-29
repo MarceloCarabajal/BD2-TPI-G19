d92ea197ab59df5586422a63bf706c4ad8b5c9a8
615374 <giselalanzillotta@gmail.com>
2026-05-29 18:15:43 -0300

    fix: recuperar version de inserts unificada y testeada en docker

--- sql/desarrollo_db/insercion_datos.sql
+++ sql/desarrollo_db/insercion_datos.sql
@@ -12,6 +12,9 @@ INSERT INTO CLASIFICACIONES (descripcion) VALUES
 ('SAM 13 - Apta para mayores de 13 años'),
 ('SAM 16 - Apta para mayores de 16 años'),
 ('SAM 18 - Apta para mayores de 18 años');
+GO
+-- SELECT * FROM CLASIFICACIONES;
+
 
 --  INSERTS PARA LA TABLA: GENEROS
 
@@ -22,6 +25,9 @@ INSERT INTO GENEROS (descripcion) VALUES
 ('Comedia'),
 ('Terror'),
 ('Animación');
+GO
+-- SELECT * FROM GENEROS;
+
 
 -- INSERTS PARA LA TABLA: PELICULAS
 
@@ -35,38 +41,12 @@ INSERT INTO PELICULAS (id_clasificacion, id_genero, titulo, sinopsis, duracion_m
 (2, 2, 'Jurassic Park', 'Un multimillonario recrea dinosaurios mediante ingeniería genética para un parque de atracciones, pero algo sale terriblemente mal.', 127),
 (1, 3, 'Rocky', 'Un boxeador de un barrio humilde recibe la oportunidad de su vida para pelear por el título mundial de peso completo.', 119);
 GO
-
-INSERT INTO CLASIFICACIONES (descripcion) VALUES
-('ATP - Apta para todo público'),
-('SAM 13 - Apta para mayores de 13 años'),
-('SAM 16 - Apta para mayores de 16 años'),
-('SAM 18 - Apta para mayores de 18 años');
-GO
--- SELECT * FROM CLASIFICACIONES;
-
-INSERT INTO GENEROS (descripcion) VALUES
-('Acción'),
-('Ciencia Ficción'),
-('Drama'),
-('Comedia'),
-('Terror'),
-('Animación');
-GO
--- SELECT * FROM GENEROS;
-
-INSERT INTO PELICULAS (id_clasificacion, id_genero, titulo, sinopsis, duracion_minutos) VALUES
-(1, 6, 'Toy Story', 'Un grupo de juguetes vivientes se meten en problemas cuando llega un nuevo juguete espacial.', 81),
-(3, 2, 'Matrix', 'Un programador de computación descubre que el mundo en el que vive es una simulación virtual.', 136),
-(2, 1, 'The Avengers', 'Los héroes más poderosos de la Tierra se unen para defender el planeta de una amenaza alienígena.', 143),
-(4, 5, 'El Conjuro', 'Investigadores paranormales acuden a ayudar a una familia que experimenta sucesos extraños en su granja.', 112),
-(1, 4, 'Shrek', 'Un ogro gruñón emprende un viaje junto a un burro parlanchín para rescatar a una princesa.', 90),
-(2, 3, 'Interestelar', 'Un grupo de científicos viaja a través de un agujero de gusano en el espacio para encontrar un nuevo hogar para la humanidad.', 169),
-(2, 2, 'Jurassic Park', 'Un multimillonario recrea dinosaurios mediante ingeniería genética para un parque de atracciones, pero algo sale terriblemente mal.', 127),
-(1, 3, 'Rocky', 'Un boxeador de un barrio humilde recibe la oportunidad de su vida para pelear por el título mundial de peso completo.', 119);
-GO
 -- SELECT * FROM PELICULAS;
 
+
 -- Gisela
+
+-- INSERTS PARA LA TABLA: COMPLEJOS
 
 INSERT INTO COMPLEJOS (nombre, direccion, telefono) VALUES
     ('CineHoyts Abasto', 'Av. Corrientes 3247, CABA', '11-4861-2200'),
@@ -76,6 +56,9 @@ INSERT INTO COMPLEJOS (nombre, direccion, telefono) VALUES
     ('Cinema Devoto', 'José Pedro Varela 4866, CABA', '11-4505-8000');
 GO
 -- SELECT * FROM COMPLEJOS;
+
+
+-- INSERTS PARA LA TABLA: SALAS
 
 INSERT INTO SALAS (id_complejo, nombre_sala, capacidad_total, tipo_sala) VALUES
     (1, 'Sala 1', 300, 'IMAX'),
@@ -87,6 +70,9 @@ INSERT INTO SALAS (id_complejo, nombre_sala, capacidad_total, tipo_sala) VALUES
 GO
 -- SELECT * FROM SALAS;
 
+
+-- INSERTS PARA LA TABLA: FUNCIONES
+
 INSERT INTO FUNCIONES (id_pelicula, id_sala, fecha_hora, precio_base) VALUES
     (1, 1, '2026-06-15 18:00:00', 4500.00), -- Película 1 en Sala 1 (IMAX)
     (2, 2, '2026-06-15 21:30:00', 3500.00), -- Película 2 en Sala 2 (3D)
@@ -97,7 +83,10 @@ INSERT INTO FUNCIONES (id_pelicula, id_sala, fecha_hora, precio_base) VALUES
 GO
 -- SELECT * FROM FUNCIONES;
 
+
 -- Henry (Parte 1)
+
+-- INSERTS PARA LA TABLA: USUARIOS
 
 INSERT INTO USUARIOS (nombre, apellido, email, password, fecha_registro) VALUES
     ('Martina', 'Gómez', 'martina.gomez@gmail.com', 'Martina123', '2025-03-15'),
@@ -109,17 +98,24 @@ INSERT INTO USUARIOS (nombre, apellido, email, password, fecha_registro) VALUES
 GO
 -- SELECT * FROM USUARIOS;
 
+
+-- INSERTS PARA LA TABLA: BUTACAS
+
 INSERT INTO BUTACAS (id_sala, fila, numero) VALUES
-    (1, 'A', 1),
-    (2, 'B', 5),
-    (3, 'A', 3),
-    (4, 'C', 7),
-    (5, 'D', 12),
-    (1, 'E', 9);
+    (1, 'A', 1), -- id_butaca = 1 (Para Función 1 - Sala 1)
+    (1, 'A', 2), -- id_butaca = 2 (Para Función 1 - Sala 1)
+    (1, 'B', 5), -- id_butaca = 3 (Para Función 1 - Sala 1)
+    (2, 'B', 6), -- id_butaca = 4 (Para Función 2 - Sala 2)
+    (3, 'A', 3), -- id_butaca = 5 (Para Función 3 - Sala 3)
+    (5, 'D', 12),-- id_butaca = 6 (Para Función 5 - Sala 5)
+    (6, 'F', 4); -- id_butaca = 7 (Para Funcion 6 - Sala 6)
 GO
 -- SELECT * FROM BUTACAS;
 
+
 -- Marcelo
+
+-- INSERTS PARA LA TABLA: METODOS_PAGOS
 
 INSERT INTO METODOS_PAGOS (nombre) VALUES
     ('Efectivo'),
@@ -130,6 +126,9 @@ INSERT INTO METODOS_PAGOS (nombre) VALUES
 GO
 -- SELECT * FROM METODOS_PAGOS;
 
+
+-- INSERTS PARA LA TABLA: RESERVAS
+
 INSERT INTO RESERVAS (id_usuario, id_funcion, fecha_reserva, total_pagado, estado) VALUES
     (1, 1, '2026-05-10 12:00:00', 17000.00, 'Pagada'),
     (2, 1, '2026-05-11 15:30:00', 8500.00, 'Pagada'),
@@ -139,6 +138,9 @@ INSERT INTO RESERVAS (id_usuario, id_funcion, fecha_reserva, total_pagado, estad
 GO
 -- SELECT * FROM RESERVAS;
 
+
+-- INSERTS PARA LA TABLA: PAGOS
+
 INSERT INTO PAGOS (id_reserva, id_metodo_pago, fecha_pago, total_pagado, estado_pago) VALUES
     (1, 3, '2026-05-10 12:05:00', 17000.00, 'Aprobado'),
     (2, 4, '2026-05-11 15:35:00', 8500.00, 'Aprobado'),
@@ -146,15 +148,18 @@ INSERT INTO PAGOS (id_reserva, id_metodo_pago, fecha_pago, total_pagado, estado_
 GO
 -- SELECT * FROM PAGOS;
 
+
 -- Henry (Parte 2)
 
+-- INSERTS PARA LA TABLA: DETALLES_RESERVAS
+
 INSERT INTO DETALLES_RESERVAS (id_reserva, id_butaca, id_funcion, precio_unitario) VALUES
-    (1, 3, 2, 5200.00),
-    (1, 4, 2, 5200.00),
-    (2, 1, 5, 7500.00),
-    (3, 6, 1, 4500.00),
-    (4, 2, 4, 4800.00),
-    (5, 5, 3, 6000.00);
+    (1, 1, 1, 4500.00), -- Reserva 1 (Función 1 - Sala 1) -> Usa Butaca 1 (Sala 1) 
+    (1, 2, 1, 4500.00), -- Reserva 1 (Función 1 - Sala 1) -> Usa Butaca 2 (Sala 1)
+    (2, 3, 1, 4500.00), -- Reserva 2 (Función 1 - Sala 1) -> Usa Butaca 3 (Sala 1)
+    (3, 5, 3, 2800.00), -- Reserva 3 (Función 3 - Sala 3) -> Usa Butaca 5 (Sala 3)
+    (4, 6, 5, 2500.00), -- Reserva 4 (Función 5 - Sala 5) -> Usa Butaca 6 (Sala 5)
+    (5, 7, 6, 3500.00); -- Reserva 5 (Función 6 - Sala 6) -> Usa Butaca 7 (Sala 6)
 GO
 -- SELECT * FROM DETALLES_RESERVAS;
 -- GO
