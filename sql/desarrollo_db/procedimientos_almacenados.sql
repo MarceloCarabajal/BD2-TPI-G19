81b9efc869ac6c46004fad34c0e37fabc8dcec7e
Henry Jose Vazquez Velasquez <henry.vazquez@alumnos.frgp.utn.edu.ar>
2026-06-20 15:32:03 -0300

    feat: Se agregan las vistas sp_ReservasPorUsuario y sp_ButacasOcupadasPorFuncion siendo probados correctamente con la ejecución de todo el script sql/desarrollo_db/procedimientos_almacenados.sql

--- sql/desarrollo_db/procedimientos_almacenados.sql
+++ sql/desarrollo_db/procedimientos_almacenados.sql
@@ -4,8 +4,6 @@ SET DATEFORMAT ymd;
 GO
 
 -- Gaston   (BD2-29)  sp_InsertarPelicula
-
-
 CREATE PROCEDURE sp_InsertarPelicula
     @ID_Clasificaciones BIGINT,
     @ID_Genero BIGINT,
@@ -117,49 +115,106 @@ BEGIN
     PRINT 'Función creada exitosamente.';
 END;
 GO
+
 -- Henry    (BD2-31)  sp_ReservasPorUsuario / sp_ButacasOcupadasPorFuncion
 CREATE PROCEDURE sp_ReservasPorUsuario
     @id_usuario BIGINT
 AS
 BEGIN
-    SELECT
-        vw_drc.id_reserva,
-        vw_drc.id_usuario,
-        vw_drc.nombre,
-        vw_drc.apellido,
-        vw_drc.email,
-        vw_drc.fecha_registro as fecha_registro_usuario,
-        vw_drc.id_funcion,
-        vw_drc.id_butaca,
-        vw_drc.precio_unitario,
-        vw_drc.total_pagado,
-        vw_drc.fecha_reserva,
-        vw_drc.estado AS estado_reserva
-    FROM vw_DetalleReservasCompleto vw_drc
-    WHERE id_usuario = @id_usuario
-    ORDER BY vw_drc.fecha_reserva DESC, vw_drc.id_reserva ASC
+    -- Validamos que el id del usuario es válido
+    IF @id_usuario < 1
+    BEGIN
+        RAISERROR('El id de usuario no es válido, recuerde que el id tiene que ser un número positivo mayor a 0.', 16, 1);
+        RETURN;
+    END
+    -- Consultamos si existe ese usuario en el sistema
+    IF NOT EXISTS (SELECT 1 FROM USUARIOS WHERE id_usuario = @id_usuario)
+    BEGIN
+        RAISERROR('El usuario especificado no existe en el sistema.', 16, 1);
+        RETURN;
+    END;
+    -- Si no existe una reserva para ese usuario, se lo notificamos
+    IF NOT EXISTS (SELECT 1 FROM RESERVAS WHERE id_usuario = @id_usuario)
+    BEGIN
+        RAISERROR('El usuario aún no tiene reservas realizadas.', 16, 1);
+        RETURN;
+    END;
+    -- Mostramos las reservas desde la más reciente a la más antigua
+    ELSE
+        SELECT
+            vw_drc.id_reserva,
+            vw_drc.id_usuario,
+            vw_drc.nombre,
+            vw_drc.apellido,
+            vw_drc.email,
+            vw_drc.fecha_registro_usuario,
+            vw_drc.id_funcion,
+            vw_drc.id_butaca,
+            vw_drc.precio_unitario,
+            vw_drc.total_pagado,
+            vw_drc.fecha_reserva,
+            vw_drc.estado_reserva
+        FROM vw_DetalleReservasCompleto vw_drc
+        WHERE id_usuario = @id_usuario
+        ORDER BY vw_drc.fecha_reserva DESC, vw_drc.id_reserva ASC;
 END;
 GO
+
+-- Pruebas BD2-31
+-- EXEC sp_ReservasPorUsuario @id_usuario = 1;
+-- GO
 
 CREATE PROCEDURE sp_ButacasOcupadasPorFuncion
     @id_funcion BIGINT
 AS
 BEGIN
-    SELECT
-        s.id_complejo,
-        s.id_sala,
-        b.id_butaca,
-        b.fila AS fila_de_butaca,
-        b.numero AS numero_de_butaca,
-        f.id_funcion,
-        f.fecha_hora
-    FROM BUTACAS b
-    LEFT JOIN SALAS s ON b.id_sala = s.id_sala
-    LEFT JOIN FUNCIONES f ON s.id_sala = f.id_sala
-    LEFT JOIN RESERVAS r ON f.id_funcion = r.id_funcion
-    WHERE r.id_reserva IS NULL
+    -- Validamos que el id de la función sea válido
+    IF @id_funcion < 1
+    BEGIN
+        RAISERROR('El id de función no es válido, recuerde que el id tiene que ser un número positivo mayor a 0.', 16, 1);
+        RETURN;
+    END
+    -- Consultamos si existe esa función en el sistema
+    IF NOT EXISTS (SELECT 1 FROM FUNCIONES WHERE id_funcion = @id_funcion)
+    BEGIN
+        RAISERROR('La función especificada no existe en el sistema.', 16, 1);
+        RETURN;
+    END;
+    -- Si no existe una reserva para ese usuario, se lo notificamos
+    IF NOT EXISTS (SELECT 1 FROM RESERVAS WHERE id_funcion = @id_funcion)
+    BEGIN
+        RAISERROR('La función aún no tiene reservas realizadas.', 16, 1);
+        RETURN;
+    END;
+    -- Mostramos las butacas por función desde la más reciente a la más antigua
+    ELSE
+        SELECT 
+            f.id_funcion,
+            f.fecha_hora as fecha_funcion,
+            p.titulo AS pelicula,
+            s.nombre_sala,
+            b.id_butaca,
+            b.fila,
+            b.numero AS numero_butaca
+        FROM FUNCIONES f
+        INNER JOIN PELICULAS p ON f.id_pelicula = p.id_pelicula
+        INNER JOIN SALAS s ON f.id_sala = s.id_sala
+        INNER JOIN BUTACAS b ON s.id_sala = b.id_sala
+        LEFT JOIN (
+            -- Subconsulta para mapear que butacas ya se vendieron para que funcion
+            SELECT r.id_funcion, dr.id_butaca 
+            FROM DETALLES_RESERVAS dr
+            INNER JOIN RESERVAS r ON dr.id_reserva = r.id_reserva
+        ) ocupadas ON f.id_funcion = ocupadas.id_funcion AND b.id_butaca = ocupadas.id_butaca
+        WHERE ocupadas.id_butaca IS NOT NULL -- Si es NULL, la butaca esta libre para esa funcion
+        AND f.id_funcion = @id_funcion
+        ORDER BY f.id_funcion ASC, fecha_funcion DESC, b.fila ASC, numero_butaca ASC; 
 END;
 GO
+
+-- Pruebas BD2-31
+--  EXEC sp_ButacasOcupadasPorFuncion @id_funcion = 1;
+-- GO
 
 -- Henry    (BD2-35)  sp_CrearReservaConDetalle  (BEGIN TRAN)
 
